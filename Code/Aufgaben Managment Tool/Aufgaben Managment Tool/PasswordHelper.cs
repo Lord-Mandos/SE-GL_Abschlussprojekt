a850fc844687ad88a45d73d68543d099c9355f47
Matthias <138225947+Lord-Mandos@users.noreply.github.com>
2025-12-04 14:25:09 +0100

    Add password hashing and verification to User
    Implemented secure password handling using PBKDF2 in PasswordHelper. User class now supports password setting and validation with salt and hash storage, improving authentication security.

--- Code/Aufgaben Managment Tool/Aufgaben Managment Tool/PasswordHelper.cs
+++ Code/Aufgaben Managment Tool/Aufgaben Managment Tool/PasswordHelper.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Security.Cryptography;
 
 namespace Aufgaben_Managment_Tool
 {
     internal class PasswordHelper
     {
+        private const int _saltSize = 16;
+        private const int _hashSize = 32;
+        private const int _iterations = 10000;
+
+        public static byte[] GenerateSalt(int size = _saltSize)
+        {
+            var saltBytes = new byte[size];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return saltBytes;
+        }
+
+        public static string HashPassword(string password, byte[] salt)
+        {
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                _iterations,
+                HashAlgorithmName.SHA256,
+                _hashSize
+            );
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool VerifyPassword(string password, byte[] salt, string passwordhash)
+        {
+            var hashToVerify = HashPassword(password, salt);
+            return hashToVerify == passwordhash;
+        }
     }
 }
--- Code/Aufgaben Managment Tool/Aufgaben Managment Tool/User.cs
+++ Code/Aufgaben Managment Tool/Aufgaben Managment Tool/User.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Spectre.Console;
 
 namespace Aufgaben_Managment_Tool
 {
     internal class User
     {
+        public string Username { get; set; }
+        public UserRole Role { get; set; }
+        public string PasswordHash { get; private set; }
+        public byte[] Salt { get; private set; }
+
+        public void SetPassword()
+        {
+            var password = AnsiConsole.Prompt<string>(
+                new TextPrompt<string>("Bitte geben Sie Ihr Passwort ein:")
+                .PromptStyle("red")
+                .Secret());
+            Salt = PasswordHelper.GenerateSalt();
+            PasswordHash = PasswordHelper.HashPassword(password, Salt);
+        }
+        public void ValidatePassword()
+        {
+            var password = AnsiConsole.Prompt<string>(
+                new TextPrompt<string>("Bitte geben Sie Ihr Passwort ein:")
+                .PromptStyle("red")
+                .Secret());
+            PasswordHelper.VerifyPassword(password, this.Salt, this.PasswordHash);
+
+        }
+
     }
 }
