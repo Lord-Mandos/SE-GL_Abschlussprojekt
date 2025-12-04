b3e9f3d51cc3918125fcc1a57a3b63bc11645e18
Matthias <138225947+Lord-Mandos@users.noreply.github.com>
2025-12-04 18:46:42 +0100

    Add CreateUser method to UserService
    Introduces a CreateUser method that prompts for username and role, validates input, sets password, and saves the new user using Spectre.Console. Provides user feedback upon successful creation.

--- Code/Aufgaben Managment Tool/Aufgaben Managment Tool/UserService.cs
+++ Code/Aufgaben Managment Tool/Aufgaben Managment Tool/UserService.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Spectre.Console;
 
 namespace Aufgaben_Managment_Tool
 {
     internal class UserService
     {
+        public static void CreateUser()
+        {
+            var repository = new UserRepository();
+            var users = repository.LoadUsers();
+
+            var user = new User();
+
+            user.Username = AnsiConsole.Prompt<string>(
+                new TextPrompt<string>("Bitte geben Sie Ihren Benutzernamen ein:")
+                .PromptStyle("green").Validate(username =>
+                {
+                    return username.Length >= 3
+                        ? ValidationResult.Success()
+                        : ValidationResult.Error("[red]Der Benutzername muss mindestens 3 Zeichen lang sein.[/]");
+                }));
+
+            user.Role = AnsiConsole.Prompt<UserRole>(
+                new SelectionPrompt<UserRole>()
+                .Title("Bitte wählen Sie Ihre Rolle:")
+                .AddChoices(UserRole.admin, UserRole.user));
+
+            user.SetPassword();
+
+            users.Add(user);
+            repository.SaveUsers(users);
+
+            AnsiConsole.MarkupLine($"[green]Benutzer {user.Username} mit Rolle {user.Role} wurde erstellt.[/]");
+        }
+
+
     }
 }
