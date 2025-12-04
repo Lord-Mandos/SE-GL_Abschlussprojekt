91a77a4ba6e7a4e462461012b675c5d0dad92854
Matthias <138225947+Lord-Mandos@users.noreply.github.com>
2025-12-04 17:58:42 +0100

    Add generic JSON load/save utility and user repository
    Refactored JsonSafeLoad to a generic class with static methods for loading and saving lists of objects to JSON files. Added UserRepository to manage user data persistence using the new generic utility.

--- Code/Aufgaben Managment Tool/Aufgaben Managment Tool/JsonSafeLoad.cs
+++ Code/Aufgaben Managment Tool/Aufgaben Managment Tool/JsonSafeLoad.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text.Json;
 
 namespace Aufgaben_Managment_Tool
 {
-    internal class JsonSafeLoad
+    internal class JsonSafeLoad<T>
     {
+        public static List<T> Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return new List<T>();
+            }
+
+            string jsonString = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<T>>(jsonString) ?? new List<T>();
+        }
+        public static void Save(string filePath, List<T> data)
+        {
+            string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, jsonString);
+        }
     }
 }
---
+++ Code/Aufgaben Managment Tool/Aufgaben Managment Tool/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Aufgaben_Managment_Tool
+{
+    internal class UserRepository
+    {
+        private const string FilePath = "users.json";
+
+        public List<User> LoadUsers()
+        {
+            return JsonSafeLoad<User>.Load(FilePath);
+        }
+
+        public void SaveUsers(List<User> users)
+        {
+            JsonSafeLoad<User>.Save(FilePath, users);
+        }
+
+    }
+}
